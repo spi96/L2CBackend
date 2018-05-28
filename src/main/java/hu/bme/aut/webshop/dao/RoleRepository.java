0c14b45a51ba85c36f50c783ee0fd6501f39a40b
Balazsi Gaspar <balazsi.gaspar@gmail.com>
2018-05-28 03:10:18 +0200

    Role törlés

--- src/main/java/hu/bme/aut/webshop/dao/RoleRepository.java
+++
@@ -1,7 +0,0 @@
-package hu.bme.aut.webshop.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RoleRepository extends JpaRepository<Role,Long> {
-    Role findByRole(String role);
-}
